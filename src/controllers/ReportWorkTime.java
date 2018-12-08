40ef8d909b042b0d23419ffb5bdf9a78e5307895
VasGor <kingstar.gvv@gmail.com>
2018-12-08 19:17:38 +0200

    thread - find managers

--- src/controllers/ReportWorkTime.java
+++ src/controllers/ReportWorkTime.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import http.Const;
+import http.HttpHandler;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import model.Employee;
 import model.Order;
+import model.User;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ReportWorkTime {
     @FXML
@@ -26,5 +34,67 @@ public class ReportWorkTime {
     @FXML
     private TextField txtFileName;
 
+    @FXML
+    private void initialize(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // 1 is the manager
+                String jsonStr = HttpHandler.getEmployeesWithPosition("1");
 
+                System.out.println(jsonStr);
+
+                if(jsonStr != null) {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    int success = jsonObj.getInt("success");
+
+                    if (success == 1) {
+                        JSONArray employeesJSON = jsonObj.getJSONArray("employees");
+                        ArrayList<Employee> employeesList = new ArrayList<>();
+                        for (int i = 0; i < employeesJSON.length(); i++) {
+                            JSONObject o = employeesJSON.getJSONObject(i);
+
+                            int userID = o.getInt(Const.EMPLOYEE_ID);
+                            String lastName = o.getString(Const.EMPLOYEE_LASTNAME);
+                            String firstName = o.getString(Const.EMPLOYEE_FIRSTNAME);
+                            String middleName = o.getString(Const.EMPLOYEE_MIDDLENAME);
+
+                            Employee employee = new Employee(lastName,firstName,middleName,userID);
+                            employeesList.add(employee);
+                        }
+
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                cbManager.getItems().clear();
+
+                                for(Employee e: employeesList)
+                                    cbManager.getItems().add(e);
+
+
+                                cbManager.setPromptText("...");
+                            }
+                        });
+
+                    } else {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                Massage.show("Что-то пошло не так",
+                                        "Неизвестная ошибка");
+                            }
+                        });
+                    }
+                } else {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            Massage.show("Что-то пошло не так",
+                                    "Проверьте соединение с сетью");
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
 }
